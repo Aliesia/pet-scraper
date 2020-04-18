5511cea04e453f01211bedc30de1dc031a11fbb4
Aliesia Pavliuk <alesia.lexy@gmail.com>
2020-04-19 00:20:36 +0200

    add mongoose

--- resultAnalysis.js
+++ resultAnalysis.js
@@ -1,5 +1,17 @@
-const compareAndSaveResults = function(dataObj) {
-    return console.log(dataObj);
+import { MONGOURI } from './data.js';
+import mongoose from 'mongoose';
+
+mongoose.connect(MONGOURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.log(err));
+
+const compareAndSaveResults = dataObj => {
+    try {
+        mongoose.disconnect();
+
+    } catch (err) {
+        console.error(err);
+    }
 };
 
 export default compareAndSaveResults;
